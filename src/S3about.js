28d5cc1cd769f9b3eee4d06d6fbda03dbbe6b6a5
Maksiks <maksiksking@gmail.com>
2024-11-12 22:38:24 +0200

    uhm yes the svg has been broken after i sent it ofc

--- src/S3about.js
+++ src/S3about.js
@@ -12,20 +12,35 @@ function S3about() {
              xmlns="http://www.w3.org/2000/svg">
             <rect width="4vw" height="61vh" fill="#212121"/>
         </svg>
-        <svg className={"blackLine1"} width="2.7vw" height="215vh" viewBox="0 0 4vw 215vh" fill="none"
-             xmlns="http://www.w3.org/2000/svg">
-            <rect width="4vw" height="95vh" fill="#212121"/>
-            <rect width="4vw" height="5vh" transform="matrix(1 0 0 -1 0 636)" fill="#1B1B1B"/>
-            <rect y="636" width="4vw" height="135vh" fill="#383737"/>
-            <rect width="4vw" height="2.75vh" transform="matrix(-1 0 0 1 52 636)" fill="black" fill-opacity="0.1"/>
+        <svg className={"blackLine1"} width="52" height="1716" viewBox="0 0 52 1716" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="52" height="598" fill="#212121"/>
+            <rect width="52" height="38" transform="matrix(1 0 0 -1 0 636)" fill="#1B1B1B"/>
+            <rect y="636" width="52" height="1080" fill="url(#paint0_linear_543_2)"/>
+            <rect width="52" height="22" transform="matrix(-1 0 0 1 52 636)" fill="black" fill-opacity="0.1"/>
+            <defs>
+                <linearGradient id="paint0_linear_543_2" x1="26" y1="636" x2="26" y2="1716"
+                                gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#383737"/>
+                    <stop offset="0.604" stop-color="#383737"/>
+                    <stop offset="0.809" stop-color="#383737" stop-opacity="0"/>
+                </linearGradient>
+            </defs>
         </svg>
+
+        {/*<svg className={"blackLine1"} width="2.7vw" height="215vh" viewBox="0 0 4vw 215vh" fill="none"*/}
+        {/*     xmlns="http://www.w3.org/2000/svg">*/}
+        {/*    <rect width="4vw" height="95vh" fill="#212121"/>*/}
+        {/*    <rect width="4vw" height="5vh" transform="matrix(1 0 0 -1 0 636)" fill="#1B1B1B"/>*/}
+        {/*    <rect y="636" width="4vw" height="135vh" fill="#383737"/>*/}
+        {/*    <rect width="4vw" height="2.75vh" transform="matrix(-1 0 0 1 52 636)" fill="black" fill-opacity="0.1"/>*/}
+        {/*</svg>*/}
 
 
         {/* layout */}
 
 
         <Card/>
-        <Skills />
+        <Skills/>
         <RightPartInAbout/>
         <BottomRect/>
 
