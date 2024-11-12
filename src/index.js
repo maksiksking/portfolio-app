673d2470c7428fabdd9f23fab2b9e76937ab3868
Maksiks <maksiksking@gmail.com>
2024-11-12 21:49:18 +0200

    As a wise man once said: "yes the phone version is jank, but is it jank enough for production?"

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {
     useRecoilValue,
 } from 'recoil';
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ReactDOM from 'react-dom/client';
 import './css/styles.scss';
 
@@ -19,6 +19,8 @@ import S4holder from "./S4holder";
 import S5webDev from "./S5webDev";
 import S6programmin from "./S6programmin";
 import S7designin from "./S7designin";
+
+
 
 function RestOfThePage() {
     return (
@@ -35,6 +37,10 @@ function RestOfThePage() {
 
 
 function Page() {
+    useEffect(() => {
+        document.title = 'Maksiks';
+    }, []);
+
     const [pass, setPass] = useState(false);
 
     const sendPass = (passdata) => {
