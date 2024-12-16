572aefda2e2c490317c9b5a9260d947096d59319
Maksiks <maksiksking@gmail.com>
2024-12-16 12:31:48 +0200

    minor fix

--- src/desiInsides/DesiRight.tsx
+++ src/desiInsides/DesiRight.tsx
@@ -12,6 +12,7 @@ interface DiscombobulateDataProps {
 }
 
 function DesiRight() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [_, setCheck] = useRecoilState(lightBoxAtom);
 
     const data = [
